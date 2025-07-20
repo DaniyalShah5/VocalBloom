f8abd195c6b4b4a4b73fe3d4f6d4a5ee21632de9
DaniyalShah5 <daniyalhcj@gmail.com>
2025-07-21 03:09:05 +0500

    fix alignment of loading

--- client/src/pages/ModuleDetails.jsx
+++ client/src/pages/ModuleDetails.jsx
@@ -278,7 +278,7 @@ const ModuleDetail = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-64">
+      <div className="text-center py-10">
         <div className='w-10 h-10 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin mx-auto'></div>
         <p>Loading module details...</p>
       </div>
