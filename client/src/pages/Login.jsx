45f5544027449a6710b5b79231bafdf689993017
DaniyalShah5 <daniyalhcj@gmail.com>
2025-07-28 11:54:54 +0500

    fix Link import

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const Login = () => {
